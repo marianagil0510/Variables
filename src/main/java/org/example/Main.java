218f3e6f64dddb28d79fc87e91405a0367585a09
MarianaGil <marianagilsalazar@gmail.com>
2023-06-25 18:50:14 -0500

    Variables

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,63 @@
+package org.example;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+       System.out.println("----------------------- practica de variables----------------------");
+        /**
+         * Existen 6 tipos de variables en java:
+         * 1. Variables numericas
+         * 2. Variables tipo caracter
+         * 3. Variables tipo booleanas
+         * 4. Variables de texto
+         * 5. Variables de fecha y tiempo
+         * 6. Variables de colecciones o listas
+         */
+        /**
+         * La forma en que se declara variables en java: <public / private> <static final> tipo de variable "nombre de la variable"
+         * Si se va a instanciar la variable con valor inicial, luego del nombre de la variable va = el valor de la variable y al final ;
+         * En caso de que no, solamente ;
+         */
+
+        //byte: representa numeros enteros de 8 bytes con signo
+        byte variable1 = 10;
+        System.out.println("Imprimiendo la variable de tipo byte: " + variable1);
+
+        /*
+         * 3 Ejemplos de cada uno (pendiente)
+         * Variables numéricas:
+
+            byte: Representa números enteros de 8 bits con signo.
+            short: Representa números enteros de 16 bits con signo.
+            int: Representa números enteros de 32 bits con signo.
+            long: Representa números enteros de 64 bits con signo.
+            float: Representa números de coma flotante de 32 bits.
+            double: Representa números de coma flotante de 64 bits.
+            Variables de caracteres:
+
+            char: Representa un único carácter Unicode de 16 bits.
+            Variables booleanas:
+
+            boolean: Representa un valor de verdadero o falso.
+            Variables de texto:
+
+            String: Representa una secuencia de caracteres.
+            StringBuilder: Utilizado para construir cadenas de caracteres de manera eficiente.
+            Variables de fecha y tiempo:
+
+            java.util.Date: Representa una fecha y hora específica.
+            java.time.LocalDate: Representa una fecha sin información de tiempo.
+            java.time.LocalTime: Representa una hora sin información de fecha.
+            java.time.LocalDateTime: Representa una fecha y hora sin información de zona horaria.
+            Variables de colecciones:
+
+            Array: Representa una colección de elementos del mismo tipo.
+            List: Representa una lista ordenada de elementos.
+            Set: Representa un conjunto de elementos únicos.
+            Map: Representa una colección de pares clave-valor.
+         */
+    }
+
+
+}
